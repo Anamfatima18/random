4d2e5aa882e3e9bbd07e5eec21c06f4c67f6fdd7
Anamfatima18 <76741606+Anamfatima18@users.noreply.github.com>
2023-09-12 11:40:03 +0500

    Added EmailVerification layout

---
+++ app/src/main/java/com/anamfatima/i201844/EmailVerification.java
@@ -0,0 +1,19 @@
+package com.anamfatima.i201844;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class EmailVerification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_verification);
+
+
+    }
+}
